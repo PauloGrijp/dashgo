61f794910dd7f9894bfa97014f4bc987db65f8d4
Paulo Xavier <pxavierf@gmail.com>
2021-11-23 14:47:09 -0300

    responsividade create user

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -26,23 +26,23 @@ export default function UserList() {
       >
         <SideBar />
 
-        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+        <Box flex="1" borderRadius={8} bg="gray.800" p={["6", "8"]}>
           <Heading size="lg" fontWeight="normal">
             Criar usuário
           </Heading>
           <Divider my="6" borderColor="gray.700" />
           <VStack spacing="8">
-            <SimpleGrid minChildWidth="240px" spacing="8" width="100%">
+            <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} width="100%">
               <Input name="name" label="Nome completo" />
               <Input name="email" label="E-mail" type="email" />
             </SimpleGrid>
-            <SimpleGrid minChildWidth="240px" spacing="8" width="100%">
+            <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} width="100%">
               <Input name="password" label="Senha" type="password" />
               <Input name="password_confirmation" label="Confirme sua senha" type="password"/>
             </SimpleGrid>
           </VStack>
 
-          <Flex mt="8" justify="flex-end">
+          <Flex mt={["6", "8"]} justify="flex-end">
             <HStack spacing="4">
               <Button colorScheme="whiteAlpha">
                 Cancelar
