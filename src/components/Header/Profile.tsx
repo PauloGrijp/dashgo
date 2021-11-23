987e8fd215a69195a4d6673fa782ffb72ab6d26b
Paulo Xavier <pxavierf@gmail.com>
2021-11-23 13:04:56 -0300

    criando responsvidade do header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Flex, Box, Text, Avatar } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex>
-      <Box mr="4" textAlign="right">
-        <Text>Paulo Xavier</Text>
-        <Text color="gray.300" fontSize="small">
-          pxavierf@gmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Paulo Xavier</Text>
+          <Text color="gray.300" fontSize="small">
+            pxavierf@gmail.com
+          </Text>
+        </Box>
+      )}
       <Avatar size="md" name="Paulo Xavier" src="https://github.com/PauloGrijp"/>
     </Flex>
   );
